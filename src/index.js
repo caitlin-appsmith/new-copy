68ba4857c1eb60f108738f1d69553ed96acc9c8c
caitlin-appsmith <96854973+caitlin-appsmith@users.noreply.github.com>
2022-04-14 07:20:25 +1000

    initial setup and data from FM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
-//Here we're importing items we'll need. You can add other imports here.
+import c3 from "c3";
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
+function loadChart () {
+const chart = c3.generate({
+  bindto: '#chart',
+  data: {
+    type: "donut",
 
-//
+    colors: {apples: "green", oranges: "orange"},
+    columns: [
+      ['apples', 90, 200, 100, 400, 150, 250],
+      ['oranges', 50, 20, 10, 40, 15, 25]
+    ]
+
+  },
+  donut: { padAngle: .05 },
+});}
+
+window.loadChart = loadChart;
+// const chart2 = c3.generate({
+//   bindto: '#chart2',
+//   data: {
+//     types: { sweets: 'bar', chocolate: 'spline' },
+//     colors: {sweets: "blue", chocolate: "purple"},
+//     columns: [
+//       ['sweets', 90, 200, 100, 400, 150, 250],
+//       ['chocolate', 50, 20, 10, 40, 15, 25]
+//     ]
+//   }
+// });
