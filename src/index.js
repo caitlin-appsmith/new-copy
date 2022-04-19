e97f6cebaa4f211ac10e743b55224424066e156c
caitlin-appsmith <96854973+caitlin-appsmith@users.noreply.github.com>
2022-04-20 00:30:58 +1000

    Added various JS functions

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ function loadChart (json) {
  
   const chartType = obj.type;
   const columns = obj.data;
+  const pattern = obj.pattern;
   console.log(columns,chartType);
 
 const chart = c3.generate({
@@ -19,17 +20,46 @@ const chart = c3.generate({
     },
     type: chartType,
 
-    colors: {apples: "green", oranges: "orange", pears: "blue", frogs: "pink"},
-    columns: columns
-
+    // colors: {apples: "green", oranges: "orange", pears: "blue", frogs: "pink"},
+    columns: columns,
+    groups: [
+      ['oranges', 'apples'],
+      ['pears']
+    ]
   },
-  
+  color: {
+    // pattern: pattern
+    pattern: ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+},
+  grid: {
+  y: {
+    show: true
+  }
+},
+regions: [
+  {axis: 'x', end: 1, class: 'regionX'},
+  {axis: 'x', start: 2, end: 4, class: 'regionX'},
+  {axis: 'x', start: 5, class: 'regionX'},
+  {axis: 'y', end: 50, class: 'regionY'},
+  {axis: 'y', start: 80, end: 140, class: 'regionY'},
+  {axis: 'y', start: 400, class: 'regionY'},
+  {axis: 'y2', end: 900, class: 'regionY2'},
+  {axis: 'y2', start: 1150, end: 1250, class: 'regionY2'},
+  {axis: 'y2', start: 1300, class: 'regionY2'},
+],
+zoom: {
+  enabled: true
+},
+gauge: {
+  max: 700
+}
 });
 window.updateChart = function (data) {
   const columns = JSON.parse (data);
   console.log(columns);
   chart.load({columns: columns, unload: ["oranges","apples"]})
 }}
+
 
 window.loadChart = loadChart;
 // const chart2 = c3.generate({
